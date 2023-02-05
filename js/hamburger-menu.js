6f04a3d5dd655372b3fc1b7c4c3c81c65994dcd3
jmathtech <jamil.matheny@gmail.com>
2023-02-04 21:22:22 -0500

    Updated the hamburger-menu.js and style.css

--- js/hamburger-menu.js
+++ js/hamburger-menu.js
@@ -1,7 +1,9 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const nav = document.querySelector('nav');
+// Hamburger menu will be activated when the user clicks button.
 
-hamburgerMenu.addEventListener('click', () => {
+const hamburgerBtn = document.querySelector('.hamburger-menu');
+const nav = document.querySelector('.nav-menu');
+
+hamburgerBtn.addEventListener('click', function () {
   console.log('hamburger menu clicked');
   nav.classList.toggle('active');
 });
